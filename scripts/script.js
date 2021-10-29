12971809bac24e779c9776243233e8677deb52ca
oakpine <58812390+oakpine@users.noreply.github.com>
2021-10-29 13:35:13 -0400

    Added comments to javascript

--- scripts/script.js
+++ scripts/script.js
@@ -4,23 +4,24 @@ $(document).ready(function(){
     $('.carousel').carousel(); // carousel
 
     // crust preference buttons
-    $('.pastry_btn').click(function(){
+    $('.pastry_btn').click(function(){ // when pastry button is clicked
         $('#graham_img').hide()
         $('#other_img').hide()
         $('#pastry_img').css('display', 'block');
     });
-    $('.graham_btn').click(function(){
+    $('.graham_btn').click(function(){ // when graham button is clicked
         $('#pastry_img').hide()
         $('#other_img').hide()
         $('#graham_img').css('display', 'block');
     });
-    $('.other_btn').click(function(){
+    $('.other_btn').click(function(){ // when other button is clicked
         $('#graham_img').hide()
         $('#pastry_img').hide()
         $('#other_img').css('display', 'block');
     });
 });
 
+// determine if element is in the viewport
 function isElementInViewport(el) {
     if (typeof jQuery == "function" && el instanceof jQuery) {
         el=el[0];
@@ -36,12 +37,15 @@ function isElementInViewport(el) {
             rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
     );
 }
+
 var scroll = window.requestAnimationFrame ||
     function(callback) {window.setTimeout(callback, 1000/60)};
-var elementsToShow = document.querySelectorAll('.show-on-scroll');
+var elementsToShow = document.querySelectorAll('.show-on-scroll'); // get elements to apply animation to
+
+// show image if it's in viewport, hide otherwise
 function loop () {
     elementsToShow.forEach(function(element) {
-        if (isElementInViewport(element)) {
+        if (isElementInViewport(element)) { 
             element.classList.add('is-visible');
         } else {
             element.classList.remove('is-visible');
